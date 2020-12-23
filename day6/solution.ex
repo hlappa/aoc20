a89c9f79df1c16287693208b139d6f8e20f25051
Aleksi Holappa <aleksi.holappa@codemate.com>
2020-12-23 02:40:02 +0200

    Day 6 part 2 done

--- day6/solution.ex
+++ day6/solution.ex
@@ -10,6 +10,24 @@ defmodule CustomCustoms do
     IO.inspect(sum)
   end
 
+  def part2 do
+    count = Enum.reduce(get_values(), 0, fn str, acc ->
+      people_count = str |> String.split(" ") |> Enum.count()
+      answered_questions_count = str |> String.replace(" ", "") |> String.graphemes() |> Enum.frequencies() |> :maps.values()
+
+      if people_count == 1 do
+        acc + String.length(str)
+      else
+        multi_answer = Enum.reduce(answered_questions_count, 0, fn q_count, acc ->
+          if q_count/people_count == 1, do: acc + 1, else: acc
+        end)
+        acc + multi_answer
+      end
+    end)
+
+    IO.puts(count)
+  end
+
   defp get_values do
     case File.read("input.txt") do
       {:ok, contents} ->
@@ -25,3 +43,5 @@ end
 
 IO.puts("Part1:")
 CustomCustoms.part1()
+IO.puts("Part2:")
+CustomCustoms.part2()
